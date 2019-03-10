8966eb1ffd55c4aa5800807c2db447a6f1d13f42
cmpmk <chamlink@hotmail.com>
2019-03-09 17:50:04 -0800

    Edits

--- extractDims.py
+++ extractDims.py
@@ -1,10 +1,12 @@
 import pencil as pc
 
+ivar = 21
+datadir='/media/cmp/Storage/research/Streaming3D/data'
 # Load in objects:
-ff = pc.read_var(trimall=True, datadir='/media/cmp/Storage/SIMovie/data')
-dim = pc.read_dim(datadir='/media/cmp/Storage/SIMovie/data')
-par = pc.read_param(datadir='/media/cmp/Storage/SIMovie/data')
-grid = pc.read_grid(datadir='/media/cmp/Storage/SIMovie/data')
+ff = pc.read_var(trimall=True, datadir=datadir, ivar=ivar)
+dim = pc.read_dim(datadir=datadir)
+par = pc.read_param(datadir=datadir)
+grid = pc.read_grid(datadir=datadir)
 
 # Parameters:
 cs0 = par.cs0
@@ -13,8 +15,7 @@ pg = par.density_power_law
 eps0 = par.eps_dtog # Dust to gas ratio
 
 # Grid variables:
-
-grids = {"Lx": grid.Lx, "Ly":grid.Ly, "Lz": grid.Lz,
-         "dx": grid.dx, "dy":grid.dy, "dz":grid.dx}
-
-print(grids)
+grids = {"Lx":grid.Lx, "Ly":grid.Ly, "Lz":grid.Lz,
+         "dx":grid.dx, "dy":grid.dy, "dz":grid.dx}
+#for key, val in grids.items():
+#    print(key, ' ', val)
