02316c5fefd031252ed58680d2c17fcfcef083a5
cmpmk <chamlink@hotmail.com>
2019-03-12 09:02:01 -0700

    Plot vorticity script

---
+++ plotVortex.py
@@ -0,0 +1,20 @@
+import pencil as pc
+from pylab import *
+
+ff = pc.read_var(trimall=True, ivar=19, magic=["vort"])
+dim = pc.read_dim()
+
+#contourf(ff.x, ff.y, ff.vort[2, int(dim.nz/2),...], 256) # Plots a low resolution image
+
+vortz_mid = ff.vort[2, int(dim.nz/2),...]
+print(vortz_mid.min())
+print(vortz_mid.max())
+
+#contourf(ff.x, ff.y, ff.vort[2, int(dim.nz/2),...], linspace(-1, 1, 256))
+
+r, phi = meshgrid(ff.x, ff.y)
+omega = 1./r**1.5
+vort_k = omega/2
+
+contourf(ff.x, ff.y, ff.vort[2, int(dim.nz/2),...]/vort_k, linspace(-1, 1, 256))
+show()
