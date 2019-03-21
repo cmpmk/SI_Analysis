db5f7ab2586d0678283b4f2f451c406dd768a630
cmpmk <chamlink@hotmail.com>
2019-03-21 10:19:29 -0700

    Moved np plot from DiskPlot to clump mass since it's more relevant

--- clumpMass.py
+++ clumpMass.py
@@ -27,3 +27,19 @@ print('Mass of clump = ', "{:.2e}".format(Mclump))
 #r0 = 5
 sigma = sigma0 * (Lx/r0)**(-pd)
 
+# def np_plot(x):
+#     ff = pc.read_var(trimall=True, ivar=x)
+#     dim = pc.read_dim()
+#     r = ff.x
+#     phi = ff.y
+#     z = ff.z
+#     r2d, p2d = meshgrid(r, phi)
+#     x = r2d*cos(p2d)
+#     y = r2d*sin(p2d)
+#     P1= contourf(ff.x, ff.y, ff.np[int(dim.nz/2),...], 256)
+#     xlabel('x')
+#     ylabel('y')
+#     title('Mass Clump Formed')
+#     cbar = colorbar(P1, orientation='horizontal')
+#     show()
+# #np_plot(21)
