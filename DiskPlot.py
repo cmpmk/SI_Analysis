565cee0e2d6c4a3a6bf4a2e07a7615b2830dbfbc
cmpmk <chamlink@hotmail.com>
2019-04-02 23:41:16 -0700

    Migrated vorticity plotting script to it's own function in DiskPlot to reduce number of scripts

--- DiskPlot.py
+++ DiskPlot.py
@@ -1,5 +1,3 @@
-import time
-start = time.time()
 import pencil as pc
 from pylab import *
 
@@ -27,21 +25,19 @@ def diskxy_xz(a):
     fig, ax = subplots(1, 2)
     fig.suptitle('Dust Density at t = %s Orbits' % a)
     ax[0].contourf(x, y, lg_rhop_xy, resxy)
-    ax[1].contourf(r, z, lg_rhop_xz, resxz)
     ax[0].set_xlim([x.min(), x.max()])
     ax[0].set_ylim([y.min(), y.max()])
+    ax[0].set_title('x-y');     ax[0].set_ylabel('y')
+    ax[1].contourf(r, z, lg_rhop_xz, resxz)
     ax[1].set_xlim([r.min(), r.max()])
     ax[1].set_ylim([z.min(), z.max()])
-    ax[0].set_title('x-y')
-    ax[1].set_title('x-z')
+    ax[1].set_title('x-z');     ax[1].set_ylabel('z')
     for i in range(len(ax)):
         ax[i].set_facecolor('black')
         ax[i].set_xlabel('x')
         ax[i].set_aspect('equal')
-    ax[0].set_ylabel('y')
-    ax[1].set_ylabel('z')
     show()
-#
+# Return a 1x3 dust density contour at each orbit
 def disk_plot_3(a, b, c):
     times = [a, b, c]
     rhops = [Disk(i) for i in times]
@@ -54,10 +50,8 @@ def disk_plot_3(a, b, c):
         axs[i].set_ylabel('y')
         axs[i].set_title('t = %s' % times[i])
     #cbar = fig.colorbar(p1, ax=axs, orientation='horizontal')
-    end = time.time()
-    print(end-start)
     show()
-#
+# Return a 1x3 dust density contour at each orbit
 def disk_plot_6(a, b, c, d, e, f):
     times = [a, b, c, d, e, f]
     rhops = [Disk(t) for t in times]
@@ -70,6 +64,26 @@ def disk_plot_6(a, b, c, d, e, f):
         axs[0,i].set_title('t = %s' % times[i])
         axs[1,i].set_title('t = %s' % times[i+3])
     show()
+# Vorticity plot
+def vort_plot(ivar):
+    ff = pc.read_var(trimall=True, ivar=ivar, magic=["vort"])
+    r = ff.x; phi = ff.y; z = ff.z
+    vort_z = ff.vort[2,...]
+    rhop = log10(ff.rhop[int(nz/2),...] + epsi)
+    rho = log10(ff.rho[int(nz/2),...] + epsi)
+    res = linspace(rhop.max()-4, rhop.max(), 256)
+    fig, ax = subplots(1, 2)
+    ax[0].contourf(r, phi, mean(vort_z, axis=0), linspace(-1, 1, 256))
+    ax[1].contourf(r, phi, rhop/rho, res) 
+    ax[0].set_title('Vorticity')
+    ax[1].set_title(r'$\rho_{p}/\rho_{g}$')
+    ax[1].set_facecolor('black')
+    for i in range(len(ax)):
+        ax[i].set_xlabel('r')
+        ax[i].set_ylabel(r'$\phi$')
+    show()
+                
+vort_plot(21)    
 #diskxy_xz(21)
-disk_plot_3(2, 15, 21) #30.5, 29.9, 30.5
+#disk_plot_3(2, 15, 21) #30.5, 29.9, 30.5
 #disk_plot_6(2, 3, 8, 12, 15, 21) #87
