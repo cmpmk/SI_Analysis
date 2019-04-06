6d0933415bbdcabd1ff58113a3c2ac061c0417e6
cmpmk <chamlink@hotmail.com>
2019-04-06 14:09:30 -0700

    compare the runs

--- dust_gas.py
+++ dust_gas.py
@@ -12,14 +12,15 @@ rho = log10(ff.rho[int(nz/2),...] + 1e-4)
 vort_z = ff.vort[2,...]
 resp = linspace(rhop.max()-4, rhop.max(), 256)
 resg = linspace(rho.min(), rho.max(), 256)
-
+dtog = rhop/rho
+rese = linspace(-1, 1, 256)
 fig, ax = subplots(1, 3, sharex=True, sharey=True)
 ax[0].contourf(r, phi, rhop, resp)
 ax[1].contourf(r, phi, rho, resg)
-ax[2].contourf(r, phi, mean(vort_z, axis=0), linspace(-1, 1, 256))
+ax[2].contourf(r, phi, dtog, rese)
 ax[0].set_title(r'$\rho_{p}$')
 ax[1].set_title(r'$\rho_{g}$')
-ax[2].set_title('Vorticity')
+ax[2].set_title(r'$\rho_{p}/\rho_{g}$')
 for i in range(len(ax)):
     ax[i].set_facecolor('black')
 fig.tight_layout()
