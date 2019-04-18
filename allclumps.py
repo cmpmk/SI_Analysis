b178fa3afb0ea8000ebb051f462d5114ab49f8f3
cmpmk <cmpmk@github.com>
2019-04-18 16:06:10 -0700

    Still working on cleaning and merging these

--- allclumps.py
+++ allclumps.py
@@ -4,7 +4,7 @@ from pylab import *
 #### Return coordinates of most massive clump:
 ff = pc.read_var(trimall=True, ivar = 21)
 dim = pc.read_dim()
-
+epsi = 1e-4
 nz = dim.nz
 # Determine the location of the n-largest masses in the disk
 #[ 987. 1001. 1053. 1054. 1088. 1098. 1134. 1374. 1865. 2053.]
@@ -19,73 +19,60 @@ ix=MM[2]
 iy=MM[1]
 iz=MM[0]
 
-print(N[31, 26, 160])
+#print(N[31, 26, 160])
 adj = 0.03
 def return_coors(X, Y, Z):
     print(r[X], phi[Y], z[Z])
 
-    
-    
-return_coors(26, 160, 31)
-
-#contourf(r, z, N[:,int(1024/2),:], 256)
-#contourf(r, phi, N[int(iz),...], 256)
-#xlim(r[ix]-adj, r[ix]+adj)
-#ylim(phi[iy]-adj, phi[iy]+adj)
-#show()
-
+return_coors(ix, iy, iz)
 
 def stack_rhops():
-#    rp = []
-    for i in range(nz):
-        rp = sum(
-#    print(shape(rp))
-#    contourf(r, phi, rp, 256)
+    rp = []
+    for i in arange(0, nz):
+        rp = ff.rhop[i,...]
+    print(amax(rp))
+    contourf(r, phi, rp, 256)
     show()
-    
-stack_rhops()
+
+def stack_nps():
+    N = []
+    for i in range(0, nz):
+        N = ff.np[i,...]
+#    contourf(r, phi, N, 256)
+    M = N.flatten()
+    n = 10 # Number of values to return
+    M = (M[argsort(M)[-n::]])
+    print(M)
+    X = array([])
+    Y = array([])
+    for i in M:
+        y, x = where(N==i)
+        X = append(X,r[x])
+        Y = append(Y,phi[y])
+    print('X = ', X)
+    print('Y = ', Y)
+    print(X[0], Y[0])
 
 
+N = []
+for i in range(0, nz):
+    N = ff.np[i,...]
 
+MM = where(N==N.max())
+print(N.shape)
+print(MM)
+lg_N = log10(N + epsi)
+res = linspace(amax(lg_N)-4, amax(lg_N), 256)
+fig, ax = subplots(1, 1)
+a = ax.contourf(r, phi, lg_N, res)
+ax.set_facecolor('black')
+cbar = colorbar(a,ax=ax, orientation='vertical')
+cbar_ticks = ["{:0.2f}".format(i) for i in linspace(amin(N),amax(N), 9)]
+cbar.ax.set_yticklabels(cbar_ticks)
+show()
 
-#M = N.flatten()
-#n = 10 # n highest values
-#M = (M[argsort(M)[-n:]])
-#print(M)
-#X = array([])
-#Y = array([])
-#for i in M:
-#    y, x = where(N==i)
-#    X = append(X,r[x])
-#    Y = append(Y,phi[y])
-
-
-# pos = 8
-# fig, ax = subplots(1,4)
-# adj = 0.02;
-# print(X)
-# print(Y)
-# for i in range(4):
-#     ax[i].contourf(r, phi, N, 256)
-#     ax[i].set_xlim(X[i]-adj, X[i]+adj)
-#     ax[i].set_ylim(Y[i]-adj, Y[i]+adj)
-#show()
-#ax.contourf(r, phi, N, 256)
-# nz = 32
-# def plot_mass():
-#     fig, axs = subplots(1, 2)
-#     P1 = axs[0].contourf(ff.x, ff.y, ff.np[int(nz/2),...], 256)
-#     cbar = colorbar(P1, orientation='horizontal')
-#     adj = 0.03
-#     axs[1].contourf(x, y, N, 256)
-#     axs[1].set_xlim([x_loc-adj, x_loc+adj])
-#     axs[1].set_ylim([y_loc-adj, y_loc+adj])
-#     show()
-#plot_mass()
-
-#x_loc = X[pos]
-#y_loc = Y[pos]
-#ax.set_xlim(x_loc-adj, x_loc+adj)
-#ax.set_ylim(y_loc-adj, y_loc+adj)
-#show()
-
+lg_Nz = log10(ff.np[:,int(ny/2),:] + epsi)
+contourf(r, z, lg_Nz, res)
+show()
+#stack_rhops()
+#stack_nps()
--- clumps.py
+++ clumps.py
@@ -47,3 +47,94 @@ print('Mass of clump (ceres) = ', "{:.2e}".format(Mclump/Mceres))
 x = ff.x; y = ff.y
 N = ff.np[int(nz/2),...]
 index = 
+
+
+
+#### Return coordinates of most massive clump:
+ff = pc.read_var(trimall=True, ivar = 21)
+dim = pc.read_dim()
+
+nz = dim.nz
+# Determine the location of the n-largest masses in the disk
+#[ 987. 1001. 1053. 1054. 1088. 1098. 1134. 1374. 1865. 2053.]
+
+r = ff.x
+phi = ff.y
+z = ff.z
+N = ff.np#[16,...]
+MM = where(N==N.max())
+print(MM)
+ix=MM[2]
+iy=MM[1]
+iz=MM[0]
+
+print(N[31, 26, 160])
+adj = 0.03
+def return_coors(X, Y, Z):
+    print(r[X], phi[Y], z[Z])
+
+    
+    
+return_coors(26, 160, 31)
+
+#contourf(r, z, N[:,int(1024/2),:], 256)
+#contourf(r, phi, N[int(iz),...], 256)
+#xlim(r[ix]-adj, r[ix]+adj)
+#ylim(phi[iy]-adj, phi[iy]+adj)
+#show()
+
+
+def stack_rhops():
+#    rp = []
+    for i in range(nz):
+        rp = sum(
+#    print(shape(rp))
+#    contourf(r, phi, rp, 256)
+    show()
+    
+stack_rhops()
+
+
+
+
+#M = N.flatten()
+#n = 10 # n highest values
+#M = (M[argsort(M)[-n:]])
+#print(M)
+#X = array([])
+#Y = array([])
+#for i in M:
+#    y, x = where(N==i)
+#    X = append(X,r[x])
+#    Y = append(Y,phi[y])
+
+
+# pos = 8
+# fig, ax = subplots(1,4)
+# adj = 0.02;
+# print(X)
+# print(Y)
+# for i in range(4):
+#     ax[i].contourf(r, phi, N, 256)
+#     ax[i].set_xlim(X[i]-adj, X[i]+adj)
+#     ax[i].set_ylim(Y[i]-adj, Y[i]+adj)
+#show()
+#ax.contourf(r, phi, N, 256)
+# nz = 32
+# def plot_mass():
+#     fig, axs = subplots(1, 2)
+#     P1 = axs[0].contourf(ff.x, ff.y, ff.np[int(nz/2),...], 256)
+#     cbar = colorbar(P1, orientation='horizontal')
+#     adj = 0.03
+#     axs[1].contourf(x, y, N, 256)
+#     axs[1].set_xlim([x_loc-adj, x_loc+adj])
+#     axs[1].set_ylim([y_loc-adj, y_loc+adj])
+#     show()
+#plot_mass()
+
+#x_loc = X[pos]
+#y_loc = Y[pos]
+#ax.set_xlim(x_loc-adj, x_loc+adj)
+#ax.set_ylim(y_loc-adj, y_loc+adj)
+#show()
+
