8bfdbf49b77cd07676747c7d2dbd1e3b604297d6
cmpmk <cmpmk@github.com>
2019-05-09 12:24:15 -0700

    Spaced out lines for function

--- clumps.py
+++ clumps.py
@@ -16,7 +16,7 @@ npar = consts['npar'] # Load number of particles
 np = dat['NP']        # Load particle density (np) vals
 
 
-# Stack all the np levels    
+# Stack all the np levels
 def stack_nps():
     nofp = zeros((ny, nx))
     for i in arange(0, nz):
@@ -34,8 +34,11 @@ def largest_locs(Array, n):
 
 cl_np = asarray(largest_locs(nofp, 10)) # assign the return as an array
 
+rloc = cl_np[1]
+ploc = cl_np[0]
+
 # Compare the stacked and unstacked np values to reassure the difference
-def plot_compare(): 
+def plot_compare():
     r2d, p2d = meshgrid(r, phi)
     x = r2d*cos(p2d)
     y = r2d*sin(p2d)
@@ -50,20 +53,37 @@ def plot_compare():
 # Convert code units to physical units for clumps
 def clump_mass(npval):
     pd = consts['pd']
+    sigma0 = 1500.
+
     unit_length = 1.49e13      # AU
     unit_column_density = 1500 # g/cm3
     dtog = 1e-4                # Assign dust to gas ratio
-    sigma0 = 1500.
-    r_ext = consts['r_ext']*unit_length
-    r_int = consts['r_int']*unit_length
-    r0 = 1.0*unit_length   # AU
+
+    r_ext = consts['r_ext'] * unit_length # Outer disk boundary
+    r_int = consts['r_int'] * unit_length # Inner disk boundary
+
+    r0 = 1.0*unit_length   # (AU) Reference radius
     rho0 = 1.0*unit_length # Initial density
+
     Mdisk = 2*pi/(2-pd) *sigma0 * (r0**(pd))*(r_ext**(2-pd) - r_int**(2-pd))
+
+    # Check if mass of gas, disk, and dust are proportional
     Mgas = Mdisk
     Mdust = Mgas * dtog
-    Mp = Mdust / npar
-    Mclump = npval*Mp
+
+    Mp = Mdust / npar # Mass per particle
+    Mclump = npval*Mp # Mass of clump (for input)
+
+    print('Mass of disk = ', "{:.2e}".format(Mdisk))
+    print('Mass per particle = ', "{:.2e}".format(Mp))
+    print('Mass of clump (g))', "{:.2e}".format(Mclump))
+    print('Mass of clump (ceres) = ', "{:.2e}".format(Mclump/Mceres))
+
     return Mclump
+
+def plot_mass():
+    ### Return coordinates of most massive clump:
+    N = NP[]
 
 # Determine roche for clump
 def Roche_Limit(mass):
