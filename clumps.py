ff61dbb8c85533263c278602e3c2b50700629e06
cmpmk <cmpmk@github.com>
2019-05-08 17:56:17 -0700

    Added function that will return indices of the n-largest values within the density array, still working on further analysis. The data is now loaded in without using pencil, which sped it up significantly. Before: 1.8 seconds, After: 0.284 seconds

--- clumps.py
+++ clumps.py
@@ -1,48 +1,58 @@
 ########## allclumps ##########
+## Full disk clump analysis ##
+import time
+start = time.time()
 from pylab import *
-import pencil as pc
+
 consts = load('saved/constants.npz')
+xyz = load('saved/xyz.npz')
+dat = load('saved/var21.npz')
 
-# Determine the location of the n-largest masses in the disk
+r, phi, z = xyz['r'], xyz['phi'], xyz['z'] # Load box arrays
 
-ff = pc.read_var(trimall=True, ivar = 21)
-epsi = 1e-4
+nx, ny, nz = consts['nx'], consts['ny'], consts['nz'] # Load grid resolution
+npar = consts['npar'] # Load number of particles
 
-r, phi, z, nz = ff.x, ff.y, ff.z, 32
-pd = consts['pd']
-npar = consts['npar']
-#ix, iy, iz = MM[2], MM[1], MM[0]
+np = dat['NP']        # Load particle density (np) vals
 
-### Return coordinates of most massive clump:
-def return_coors(X, Y, Z):
-    print(r[X], phi[Y], z[Z])
-    return_coors(ix, iy, iz)
 
 # Stack all the np levels    
 def stack_nps():
-    nofp = zeros((1024, 384))
+    nofp = zeros((ny, nx))
     for i in arange(0, nz):
-        nofp += ff.np[i,...]
+        nofp += np[i,...]
     return nofp
 
 nofp = stack_nps()
-r2d, p2d = meshgrid(ff.x, ff.y)
-x = r2d*cos(p2d)
-y = r2d*sin(p2d)
-fig, ax = subplots(1,2)
-nofp10 = log10(nofp + 1e-4)
-ax[0].contourf(x, y, nofp, 256)
-ax[0].set_aspect('equal')
-ax[1].contourf(x, y, ff.np[16,...], 256)
-ax[1].set_aspect('equal')
-show()
-A = list(map(max, nofp))
+print(amax(nofp))
+
+def largest_locs(Array, n):
+    flat = Array.flatten()
+    indices = argpartition(flat, -n)[-n:]
+    indices = indices[argsort(-flat[indices])]
+    return unravel_index(indices, Array.shape)
+
+cl_np = asarray(largest_locs(nofp, 10)) # assign the return as an array
+
+# Compare the stacked and unstacked np values to reassure the difference
+def plot_compare(): 
+    r2d, p2d = meshgrid(r, phi)
+    x = r2d*cos(p2d)
+    y = r2d*sin(p2d)
+    fig, ax = subplots(1,2)
+    nofp10 = log10(nofp + 1e-4)
+    ax[0].contourf(x, y, nofp, 256)
+    ax[0].set_aspect('equal')
+    ax[1].contourf(x, y, np[16,...], 256)
+    ax[1].set_aspect('equal')
+    show()
 
 # Convert code units to physical units for clumps
 def clump_mass(npval):
+    pd = consts['pd']
     unit_length = 1.49e13      # AU
     unit_column_density = 1500 # g/cm3
-    dtog = 1e-4
+    dtog = 1e-4                # Assign dust to gas ratio
     sigma0 = 1500.
     r_ext = consts['r_ext']*unit_length
     r_int = consts['r_int']*unit_length
@@ -64,7 +74,6 @@ def Roche_Limit(mass):
     d = R*(2*(M/m))**(1./3)
     return d
 
-#a = Roche_Limit(2053)
 
-#print(a/(1.496e8))
-
+end = time.time()
+print('Time taken = ', round(end-start, 3))
