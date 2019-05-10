5e61f43003dc47369d2803d508ad48b587daa2bb
cmpmk <cmpmk@github.com>
2019-05-09 18:18:33 -0700

    Merged clumpMass script into this script (again) except now it's a class

--- clumps.py
+++ clumps.py
@@ -43,37 +43,69 @@ cl_rhop = asarray(largest_locs(nrhop, 10)) # assign the ff.rhop return as an arr
 rloc = cl_np[1] # r-coordinates
 ploc = cl_np[0] # phi-coordinates
 
+# Convert code units to physical units
+class clumpMass():
+    def __init__(self, npvalue):
+        self.npvalue = npvalue
 
-# Convert code units to physical units for clumps
-def clump_mass(npval):
-    pd = consts['pd']
-    sigma0 = 1500.
+    def ConvertUnits(self):
+        r_ext = consts['r_ext']; r_int = consts['r_int']
+        pd = consts['pd']
 
-    unit_length = 1.49e13      # AU
-    unit_column_density = 1500 # g/cm3
-    dtog = 1e-4                # Assign dust to gas ratio
+        # Minimum mass solar nebula
+        # Sigma = 1500 (r/AU)**(-1.5) g/cm3
+        # T = 280/sqrt(r/AU) K
+        # T0 = 280, Omega0 = sqrt(G*Msun/AU ** 1.5), H0 = cs0/Omega0, rho0 = sigma0/H0
+        sigma0 = 1500.
 
-    r_ext = consts['r_ext'] * unit_length # Outer disk boundary
-    r_int = consts['r_int'] * unit_length # Inner disk boundary
+        unit_length = 1.49e13      # AU
+        unit_column_density = 1500 # g/cm3
+        dtog = 1e-4                # Assign dust to gas ratio
 
-    r0 = 1.0*unit_length   # (AU) Reference radius
-    rho0 = 1.0*unit_length # Initial density
+        r_ext = r_ext * unit_length # Outer disk boundary
+        r_int = r_int * unit_length # Inner disk boundary
 
-    Mdisk = 2*pi/(2-pd) *sigma0 * (r0**(pd))*(r_ext**(2-pd) - r_int**(2-pd))
+        r0 = 1.0*unit_length   # (AU) Reference radius
+        rho0 = 1.0*unit_length # Initial density
 
-    # Check if mass of gas, disk, and dust are proportional
-    Mgas = Mdisk
-    Mdust = Mgas * dtog
+        Mdisk = 2*pi/(2-pd) *sigma0 * (r0**(pd))*(r_ext**(2-pd) - r_int**(2-pd))
 
-    Mp = Mdust / npar # Mass per particle
-    Mclump = npval*Mp # Mass of clump (for input)
+        # Check if mass of gas, disk, and dust are proportional
+        Mgas = Mdisk
+        Mdust = Mgas * dtog
 
-    print('Mass of disk = ', "{:.2e}".format(Mdisk))
-    print('Mass per particle = ', "{:.2e}".format(Mp))
-    print('Mass of clump (g))', "{:.2e}".format(Mclump))
-    print('Mass of clump (ceres) = ', "{:.2e}".format(Mclump/Mceres))
+        Mp = Mdust / npar # Mass per particle
+        Mclump = self.npvalue*Mp # Mass of clump (for input)
+        Mceres = 8.958e23
+        print('Mass of disk = ', "{:.2e}".format(Mdisk))
+        print('Mass per particle = ', "{:.2e}".format(Mp))
+        print('Mass of clump (g))', "{:.2e}".format(Mclump))
+        print('Mass of clump (ceres) = ', "{:.2e}".format(Mclump/Mceres))
 
-    return Mclump
+
+    def plot_mass():
+        # Return coordinates of most massive clump and plot zoomed in image
+        N = np[int(nz/2)]
+        index = unravel_index(argmax(N, axis=None), N.shape) # Get largest value from ff.np array
+        x_loc = r[index[1]]   # Grab the x value from the index
+        y_loc = phi[index[0]] # Grab the y value from the index
+        adj = 0.03            # Used to limit axes to mimic zooming into the mass
+
+        fig, axs = subplots(1, 2)
+        fig.suptitle('Largest Located Clump')
+        P1 = axs[0].contourf(r, phi, N, 256) # Plot the entire region
+        axs[1].contourf(r, phi, N, 256)
+        axs[1].set_xlim([x_loc-adj, x_loc+adj])
+        axs[1].set_ylim([y_loc-adj, y_loc+adj])
+        cbar = colorbar(P1, orientation='horizontal')
+        for i in range(len(axs)):
+            axs[i].set_xlabel('r')
+            axs[i].set_ylabel(r'$\phi$')
+        show()
+
+p1 = clumpMass(2053)
+p1.ConvertUnits()
+clumpMass.plot_mass()
 
 def Hill_Radius(Mclump):
     M0 = 2000 # Solar mass in code units
@@ -82,14 +114,14 @@ def Hill_Radius(Mclump):
     return Rh
 
 
-# Determine roche densities 
+# Determine roche densities
 def Roche_Limit(R):
     M0 = 2000 # Solar mass in code units
     rhor = (3/(2*pi))*(M0 / R**3)
     return rhor
 
 rhor = Roche_Limit(r) # Create an array of roche densities for each point along the radius
-print(rhor)
+#print(rhor)
 
 # Compare the stacked and unstacked np values to reassure the difference
 def plot_compare():
